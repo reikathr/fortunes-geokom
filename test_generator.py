d93920770ae9a634011256aaf92b51c098210c49
meteoreinger <reikathr@gmail.com>
2024-11-17 13:32:12 +0700

    added simple py file to create random coordinates

---
+++ test_generator.py
@@ -0,0 +1,8 @@
+# Change for points needed
+points = 30
+
+coordinates = [(random.randint(0, 500), random.randint(0, 500)) for i in range(points)]
+
+# Format and print the coordinates
+formatted_coordinates = "\n".join(f"{x} {y}" for x, y in coordinates)
+print(formatted_coordinates)
